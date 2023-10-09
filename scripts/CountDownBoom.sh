851eedcc1e36d1b82ce718488285ce4ecf0e57d4
mmariam-ali <146266404+mmariam-ali@users.noreply.github.com>
2023-10-09 17:56:45 +0000

    scripts

---
+++ scripts/CountDownBoom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Count down from 10 to 0 and then display Boom
+for ((counter=10; counter>=0; counter--)) do
+echo $counter
+done
+echo "BOOOMMMMM *exploshuun*"
---
+++ scripts/CreateFile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+#Create 3 files and let the user name and enter information into them
